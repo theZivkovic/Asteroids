7065409dcb82621d784937e83eb672f07edca3ca
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2025-02-07 11:16:55 +0100

    config cooldown to 3s

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ const config = {
         bodyColor: 0xFFFFFF,
         cooldownColor: 0xFF0000,
         fireColor: 0xFFFF00,
-        cooldownTimeMs: 5000,
+        cooldownTimeMs: 3000,
         acceleration: 0.15,
         maxSpeed: 6
     },
